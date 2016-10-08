45d127524da3ed36721d330dca7a91589b7358b2
qizhenghao <qizhenghao258@163.com>
2016-10-08 20:13:20 +0800

    ...

--- jdM/js/index.js
+++ jdM/js/index.js
@@ -234,5 +234,41 @@ function downTime(){
 
     },1000);
 
+/*倒计时*/
+function downTime1(){
+    /*需要倒计时的时间*/
+
+    var time = 5 * 60 * 60 ;
+    var timer = null;
+
+    /*操作dom*/
+    var skTime = document.querySelector('.sk_time');
+    /*所有的SPAN*/
+    var spans = skTime.querySelectorAll('span');
+
+    timer = setInterval(function(){
+        if(time <= 0){
+            clearInterval(timer);
+            return false;
+        }
+        time -- ;
+
+        /*格式化*/
+        var h = Math.floor(time/3600);
+        var m = Math.floor(time%3600/60);
+        var s = time%60;
+
+        console.log(h + ', ' + m + ', ' + s);
+
+        spans[0].innerHTML = Math.floor(h/10);
+        spans[1].innerHTML = h%10;
+
+        spans[3].innerHTML = Math.floor(m/10);
+        spans[4].innerHTML = m%10;
+
+        spans[6].innerHTML = Math.floor(s/10);
+        spans[7].innerHTML = s%10;
+
+    },1000);
 
 }
