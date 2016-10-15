9e652b1c98f605f218b63f10d1d1bae2a12594dd
qizhenghao <qizhenghao258@163.com>
2016-10-15 20:27:07 +0800

    add sixth day js

--- RenrenH5/js/common.js
+++ RenrenH5/js/common.js
@@ -70,3 +70,54 @@ Date.prototype.Format = function (fmt) { //author: meizz
     // }
  }
 
+
+/** 
+ * 模仿android里面的Toast效果，主要是用于在不打断程序正常执行的情况下显示提示数据 
+ * @param config 
+ * @return 
+ */  
+var Toast = function(config){  
+    this.context = config.context==null?$('body'):config.context;//上下文  
+    this.message = config.message;//显示内容  
+    this.time = config.time==null?3000:config.time;//持续时间  
+    this.left = config.left;//距容器左边的距离  
+    this.top = config.top;//距容器上方的距离  
+    this.init();  
+}  
+var msgEntity;  
+Toast.prototype = {  
+    //初始化显示的位置内容等  
+    init : function(){  
+        $("#toastMessage").remove();  
+        //设置消息体  
+        var msgDIV = new Array();  
+        msgDIV.push('<div id="toastMessage">');  
+        msgDIV.push('<span>'+this.message+'</span>');  
+        msgDIV.push('</div>');  
+        msgEntity = $(msgDIV.join('')).appendTo(this.context);  
+        //设置消息样式  
+        var width1 = this.context.width();
+        var width2 = msgEntity.find('span').width()/2;
+        var left = this.left == null ? this.context.width()/2-msgEntity.find('span').width()/4 : this.left;  
+        var top = this.top == null ? '48%' : this.top;  
+        msgEntity.css({position:'absolute',top:top,'z-index':'99',left:left,'background-color':'black',color:'white','font-size':'0.33rem',padding:'0.17rem',margin:'0.17rem','border-radius':'0.5rem'});  
+        msgEntity.hide();  
+    },  
+    //显示动画  
+    show :function(){  
+        msgEntity.fadeIn(this.time/2);  
+        msgEntity.fadeOut(this.time/2);  
+    }           
+}  
+
+function compareDate(a, b) {
+    var arr = a.split("-");
+    var starttime = new Date(arr[0], arr[1], arr[2]);
+    var starttimes = starttime.getTime();
+
+    var arrs = b.split("-");
+    var lktime = new Date(arrs[0], arrs[1], arrs[2]);
+    var lktimes = lktime.getTime();
+
+    return starttimes >= lktimes;
+}
--- RenrenH5/js/guard.js
+++ RenrenH5/js/guard.js
@@ -21,6 +21,7 @@ function setName(){
     // console.log(window.pay + '');
     
     // var pay = window.pay;
+    var fmt = "yyyy-MM-dd";
     var userName = document.querySelector('.user-name-text');
     userName.innerText = guardInfo.guardName;
     var anchor_name = document.querySelector('.anchor_name');
@@ -32,19 +33,23 @@ function setName(){
     var anchor_guarding_time = document.querySelector('.anchor_guarding_time');
 
     var validTimeArr = document.querySelectorAll('.term_validity_time');
+    var term_validity_str = document.querySelector('.term_validity_str');
     var term_validity_str1 = document.querySelector('.term_validity_str1');
     var selectedGuo = document.querySelector('.guard_time_guo');
 
-    var now = 0; 
+    var now = new Date();
+    now.setFullYear(now.getFullYear()+3);
+    var threeYearLater = now.Format(fmt);
     if ("0" == guardInfo.endTime) {
         now = new Date();
+        document.querySelector('.anchor_info').style.paddingBottom = '0.4rem';
     } else {
         anchor_guarding.style.display = 'block';
         anchor_guarding_time.innerText = guardInfo.endTime;
         now = new Date(guardInfo.endTime);
         validTimeArr[0].style.display = 'none';
         term_validity_str1.style.display = 'none';
-        document.querySelector('.term_validity_str').innerText = '继续开通守护将延至 ';
+        term_validity_str.innerText = '继续开通守护将延至 ';
     }
     var nowStr = now.Format("yyyy-MM-dd"); 
 
@@ -53,9 +58,16 @@ function setName(){
    
      var priceDomArr = document.querySelectorAll('.price');
      var monthDomArr = document.querySelectorAll('.month');
+     var discountArr = document.querySelectorAll('.guard_discount');
      for (var i = 0; i < payList.length ; i++) {
         priceDomArr[i].innerText = payList[i].price * payList[i].months;
         monthDomArr[i].innerText = payList[i].months;
+        if (i > 0) {
+            discountArr[i-1].innerText = payList[i].totalPrice;
+            var img = document.createElement('img');
+            img.src = 'http://s.xnimg.cn/wap/static/h5/guard/image/renrenguo_gold_icon.png';
+            discountArr[i-1].appendChild(img);
+        }
      }
      
 
@@ -72,27 +84,58 @@ function setName(){
 
 
     var selected = function(d, n) {
-        currIndex = n;
-        d.style.borderColor = "#fdd600";
+        
         var dayCount = payList[n].months * 31;
         var nextDate = addDate(now, dayCount);
         var nextDateStr = nextDate.Format("yyyy-MM-dd");
-        validTimeArr[1].innerText = nextDateStr;
-        selectedGuo.innerText = payList[n].totalPrice * 10;
+        if (compareDate(nextDateStr, threeYearLater)) {
+            new Toast({context:$('body'),message:'守护同一主播最长为3年', left:'29%'}).show(); 
+            selectedGuo.innerText = 0;
+        } else {
+            currIndex = n;
+            d.style.borderColor = "#fdd600";
+            validTimeArr[1].innerText = nextDateStr;
+            selectedGuo.innerText = payList[n].totalPrice * 10;
+        }
     }
 
     var unSelected = function(d) {
-        d.style.borderColor = "#DDDEE3";
+        d.style.borderColor = "#F2F2F2";
     }
-     var guardDiv = document.querySelectorAll('.guard_time_list_div > ul > li >div');
+     var guardDiv = document.querySelectorAll('.guard_time_list_div > ul > li >div:first-of-type');
 
-     selected(guardDiv[0], 0);
+     var setGuardDivGray = function(i) {
+        guardDiv[i].style.backgroundColor = '#F2F2F2'
+        guardDiv[i].querySelector('p > img').src = 'http://s.xnimg.cn/wap/static/h5/guard/image/renrenguo_gray_icon.png';
+     }
+
+     if (!compareDate(addDate(guardInfo.endTime, payList[0].months * 31).Format(fmt), threeYearLater)) {
+        selected(guardDiv[0], 0);
+     } else {
+        setGuardDivGray(0);
+        selectedGuo.innerText = 0;
+        term_validity_str.innerText = '无法继续开通守护了, 最长守护为3年';
+     }
      unSelected(guardDiv[1]);
      unSelected(guardDiv[2]);
      guardDiv[0].querySelector('p:first-of-type').style.textDecoration = "none";
      validTimeArr[0].innerText = nowStr;
 
-     var a = 1;
+     var initDiscount = function() {
+        if (compareDate(addDate(guardInfo.endTime, payList[2].months * 31).Format(fmt), threeYearLater)) {
+            setDiscountDomGray(discountArr[1], 2);
+            if (compareDate(addDate(guardInfo.endTime, payList[1].months * 31).Format(fmt), threeYearLater)) {
+                setDiscountDomGray(discountArr[0], 1);
+            } 
+        } 
+     }
+     var setDiscountDomGray = function(dom, i) {
+        dom.style.backgroundImage = 'url(http://s.xnimg.cn/wap/static/h5/guard/image/select_down_gray_icon.png)';
+        dom.style.color = '#C8C8C8';
+        dom.querySelector('img').src = 'http://s.xnimg.cn/wap/static/h5/guard/image/renrenguo_gray_icon.png';
+        setGuardDivGray(i);
+     }
+    initDiscount();
 
      guardDiv[0].addEventListener('click', function(event) {
             for (var j= 0; j < guardDiv.length; j++) {
@@ -123,7 +166,12 @@ function setName(){
          } , false);
 
      var openBtn = document.querySelector('button');
-     openBtn.addEventListener('click', function(event) {
+     openBtn.addEventListener('click', function(event) { 
+
+        if (compareDate(addDate(guardInfo.endTime, payList[currIndex].months * 31).Format(fmt), threeYearLater)) {
+            new Toast({context:$('body'),message:'守护同一主播最长为3年', left:'29%'}).show(); 
+            return;
+        } 
 
         function success(resultStr) {
             var result = new Function("return" + resultStr)();
@@ -136,47 +184,33 @@ function setName(){
                 if ('0' == guardInfo.endTime) {
                     anchor_guarding.style.display = 'block';
                     validTimeArr[0].style.display = 'none';
-                    document.querySelector('.term_validity_str').innerText = '继续开通守护将延至 ';
+                    term_validity_str.innerText = '继续开通守护将延至 ';
                 } else {
                     now = new Date(guardInfo.endTime);
                 }
                 guardInfo.endTime = addDate(now, dayCount).Format("yyyy-MM-dd");
                 anchor_guarding_time.innerText = guardInfo.endTime;
-
-                validTimeArr[1].innerText = addDate(new Date(guardInfo.endTime), payList[currIndex].months * 31);
-                if (isInvisible) {
-
+                now = new Date(guardInfo.endTime);
+                validTimeArr[1].innerText = addDate(now, payList[currIndex].months * 31).Format("yyyy-MM-dd");
+                document.querySelector('.anchor_info').style.paddingBottom = '0.1rem';
+                if ("false" != isInvisible) {
+                    window.wxc.xcConfirm("您目前已开启隐身查看直播功能，守护骑士特效无法展示，是否关闭隐身功能？", "关闭隐身", "暂不关闭", "custom", {
+                        title:"温馨提示",
+                        onOk:function(){
+                            window.location.href = "renrenaction://purchase";
+                    }});
                 }
+                new Toast({context:$('body'),message:'购买成功', left:'40%'}).show(); 
+                initDiscount();
             } else {
-
+                new Toast({context:$('body'),message:'未知错误', left:'40%'}).show(); 
             }
         }
 
         function fail(code) {
-             // new ConfirmBox({
-             //        text: "主人，你的人人果数量貌似不够支付" + name +"哦～",
-             //        btnInfo: [{text: "狠心离开"},{text:"跑去充值", callBack: function(){
-             //            window.location.href = "renrenaction://purchase";
-             //        }}]
-             //    });
-             
-             // $.confirm({
-             //        'title': "Title",
-             //        'message': "Message",
-             //        'buttons': {
-             //            OkBtnCaption: {
-             //                'class': 'blue',
-             //                'action': function () { $('#' + ButtonID).click(); },
-             //                'id': 'okBtn'
-             //            },
-             //            CancelBtnCaption: {
-             //                'class': 'gray',
-             //                'action': function () { },
-             //                'id': 'cancelBtn'
-             //            }
-             //       }
-             //    });
-            
+             window.wxc.xcConfirm("主人，你的人人果数量貌似不够支付这个守护哦", "跑去充值", "狠心离开", "custom", {onOk:function(){
+                window.location.href = "renrenaction://purchase";
+             }});
         }
         var request = new XMLHttpRequest();
         request.onreadystatechange = function () { // 状态发生变化时，函数被回调
@@ -201,4 +235,236 @@ function setName(){
         lockIndex = currIndex;
         request.send(postDataFormat(param));
      }, false);
+
+
+
+
+
+
+
+ /*
+ * 使用说明:
+ * window.wxc.Pop(popHtml, [type], [option ',' ])
+ * popHtml:html字符串
+ * type:window.wxc.xcConfirm.typeEnum集合中的元素
+ * options:扩展对象
+ * 用法:
+ * 1. window.wxc.xcConfirm("我是弹窗<span>lalala</span>");
+ * 2. window.wxc.xcConfirm("成功","success");
+ * 3. window.wxc.xcConfirm("请输入","input",{onOk:function(){}})
+ * 4. window.wxc.xcConfirm("自定义",{title:"自定义"})
+ */
+(function($){
+    window.wxc = window.wxc || {};
+    window.wxc.xcConfirm = function(popHtml, okStr, cancelStr, type, options) {
+        var btnType = window.wxc.xcConfirm.btnEnum;
+        var eventType = window.wxc.xcConfirm.eventEnum;
+        var popType = {
+            info: {
+                title: "信息",
+                icon: "0 0",//蓝色i
+                btn: btnType.ok
+            },
+            success: {
+                title: "成功",
+                icon: "0 -48rem",//绿色对勾
+                btn: btnType.ok
+            },
+            error: {
+                title: "错误",
+                icon: "-48rem -48rem",//红色叉
+                btn: btnType.ok
+            },
+            confirm: {
+                title: "提示",
+                icon: "-48rem 0",//黄色问号
+                btn: btnType.okcancel
+            },
+            warning: {
+                title: "警告",
+                icon: "0 -96rem",//黄色叹号
+                btn: btnType.okcancel
+            },
+            input: {
+                title: "输入",
+                icon: "",
+                btn: btnType.ok
+            },
+            custom: {
+                title: "",
+                icon: "",
+                btn: btnType.okcancel
+            }
+        };
+        var itype = type ? type instanceof Object ? type : popType[type] || {} : {};//格式化输入的参数:弹窗类型
+        var config = $.extend(true, {
+            //属性
+            title: "", //自定义的标题
+            icon: "", //图标
+            btn: btnType.ok, //按钮,默认单按钮
+            //事件
+            onOk: $.noop,//点击确定的按钮回调
+            onCancel: $.noop,//点击取消的按钮回调
+            onClose: $.noop//弹窗关闭的回调,返回触发事件
+        }, itype, options);
+        
+        var $txt = $("<p>").html(popHtml);//弹窗文本dom
+        var $tt = $("<span>").addClass("tt").text(config.title);//标题
+        var icon = config.icon;
+        var $icon = icon ? $("<div>").addClass("bigIcon").css("backgroundPosition",icon) : "";
+        var btn = config.btn;//按钮组生成参数
+        
+        var popId = creatPopId();//弹窗索引
+        
+        var $box = $("<div>").addClass("xcConfirm");//弹窗插件容器
+        var $layer = $("<div>").addClass("xc_layer");//遮罩层
+        var $popBox = $("<div>").addClass("popBox");//弹窗盒子
+        var $ttBox = $("<div>").addClass("ttBox");//弹窗顶部区域
+        var $txtBox = $("<div>").addClass("txtBox");//弹窗内容主体区
+        var $btnArea = $("<div>").addClass("btnArea");//按钮区域
+        
+        var $ok = $("<a>").addClass("sgBtn").addClass("ok").text(okStr);//确定按钮
+        var $cancel = $("<a>").addClass("sgBtn").addClass("cancel").text(cancelStr);//取消按钮
+        var $input = $("<input>").addClass("inputBox");//输入框
+        var $clsBtn = $("<a>").addClass("clsBtn");//关闭按钮
+        
+        //建立按钮映射关系
+        var btns = {
+            cancel: $cancel,
+            ok: $ok
+        };
+        
+        init();
+        
+        function init(){
+            //处理特殊类型input
+            if(popType["input"] === itype){
+                $txt.append($input);
+            }
+            
+            creatDom();
+            bind();
+        }
+        
+        function creatDom(){
+            if ('' != config.title) {
+                $popBox.append($ttBox.append($tt));
+            }
+            $popBox.append(
+            //     $ttBox.append(
+            //         $clsBtn
+            //     ).append(
+            //         $tt
+            //     )
+            // ).append(
+                $txtBox.append($icon).append($txt)
+            ).append(creatBtnGroup(btn)
+            );
+            $box.attr("id", popId).append($layer).append($popBox);
+            $("body").append($box);
+            if ('' != config.title) {
+                document.querySelector('.popBox').style.height = '4rem';
+                document.querySelector(".txtBox > p").style.paddingTop = '0.1rem';
+            } else {
+                document.querySelector('.popBox').style.height = '3.2rem';
+                document.querySelector(".txtBox > p").style.paddingTop = '0.4rem';
+            }
+        }
+        
+        function bind(){
+            //点击确认按钮
+            $ok.click(doOk);
+            
+            //回车键触发确认按钮事件
+            $(window).bind("keydown", function(e){
+                if(e.keyCode == 13) {
+                    if($("#" + popId).length == 1){
+                        doOk();
+                    }
+                }
+            });
+            
+            //点击取消按钮
+            $cancel.click(doCancel);
+            
+            //点击关闭按钮
+            $clsBtn.click(doClose);
+        }
+
+        //确认按钮事件
+        function doOk(){
+            var $o = $(this);
+            var v = $.trim($input.val());
+            if ($input.is(":visible"))
+                config.onOk(v);
+            else
+                config.onOk();
+            $("#" + popId).remove(); 
+            config.onClose(eventType.ok);
+        }
+        
+        //取消按钮事件
+        function doCancel(){
+            var $o = $(this);
+            config.onCancel();
+            $("#" + popId).remove(); 
+            config.onClose(eventType.cancel);
+        }
+        
+        //关闭按钮事件
+        function doClose(){
+            $("#" + popId).remove();
+            config.onClose(eventType.close);
+            $(window).unbind("keydown");
+        }
+        
+        //生成按钮组
+        function creatBtnGroup(tp){
+            var $bgp = $("<div>").addClass("btnGroup");
+            $.each(btns, function(i, n){
+                if( btnType[i] == (tp & btnType[i]) ){
+                    $bgp.append(n);
+                }
+            });
+            return $bgp;
+        }
+
+        //重生popId,防止id重复
+        function creatPopId(){
+            var i = "pop_" + (new Date()).getTime()+parseInt(Math.random()*100000);//弹窗索引
+            if($("#" + i).length > 0){
+                return creatPopId();
+            }else{
+                return i;
+            }
+        }
+    };
+    
+    //按钮类型
+    window.wxc.xcConfirm.btnEnum = {
+        ok: parseInt("0001",2), //确定按钮
+        cancel: parseInt("0010",2), //取消按钮
+        okcancel: parseInt("0011",2) //确定&&取消
+    };
+    
+    //触发事件类型
+    window.wxc.xcConfirm.eventEnum = {
+        ok: 1,
+        cancel: 2,
+        close: 3
+    };
+    
+    //弹窗类型
+    window.wxc.xcConfirm.typeEnum = {
+        info: "info",
+        success: "success",
+        error:"error",
+        confirm: "confirm",
+        warning: "warning",
+        input: "input",
+        custom: "custom"
+    };
+
+})(jQuery);
+
 }
